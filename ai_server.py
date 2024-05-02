5dbd01d32a0c9ef4dd3afb2db2e80395245c142b
잉여개발기 (SPDV) <min08101@naver.com>
2024-05-02 10:09:38 +0900

    host 변경

--- ai_server.py
+++ ai_server.py
@@ -36,6 +36,6 @@ def results_to_json(results, model):
 if __name__ == '__main__':
     uvicorn.run(
         app = app,
-        host = 'localhost', 
+        host = '0.0.0.0', 
         port = 3003
     )
