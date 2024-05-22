d2fa8205ecbc515ccad1eb4cf0d07f04e1db89e6
lee <skystar9927@naver.com>
2024-05-22 15:22:56 +0900

    AllPosts 디자인 적용
    어드민 기능 추가 필요

---
+++ front/src/components/AllPostsComponents/Popup.js
@@ -0,0 +1,222 @@
+import axios from "axios";
+import React, { useState, useEffect, useCallback } from 'react';
+import styled from "styled-components";
+import LoadingSpinner from "../Loading";
+
+const PopupContainer = ({ postId, setPostId, handlePopup }) => {
+    const [popupContents, setPopupContents] = useState(null);
+    const [errorLog, setErrorLog] = useState(null); 
+    const [loading, setLoading] = useState(true);
+
+    const fetchData = useCallback(async () => {
+        try {
+            setLoading(true);
+            const token = localStorage.getItem('token');
+            const response = await axios.get(`https://api.capserver.link/posts/${postId}`, {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+            setPopupContents(response.data);
+            console.log("setCon")
+        } catch (error) {
+            setErrorLog(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }, [postId]);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
+    useEffect(() => {
+        if (errorLog) {
+            console.log(errorLog);
+        }
+    }, [errorLog]);
+
+    const handleClose = () => {
+        setPostId("")
+        setPopupContents(null);
+        handlePopup();
+    }
+
+    const formatPostTime = (time) => {
+        const date = new Date(time);
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        const hour = date.getHours();
+        const minute = date.getMinutes();
+        return `${year}.${month < 10 ? '0' + month : month}.${day < 10 ? '0' + day : day} ${hour < 10 ? '0' + hour : hour}:${minute < 10 ? '0' + minute : minute}`;
+    };
+
+    const formatPrice = (price) => {
+        return new Intl.NumberFormat().format(price);;
+    };
+
+    const handleOverlayClick = (e) => {
+        if (e.target === e.currentTarget) {
+            handleClose();
+        }
+    };
+
+
+    return (
+        <PopupPresentation
+            paid={popupContents?.paid}
+            accepted={popupContents?.accepted}
+            time={popupContents?.time}
+            userName={popupContents?.userName}
+            //phoneNumber={popupContents?.phoneNumber} // 추가 필요
+            garbageName={popupContents?.garbageName}
+            garbageContent={popupContents?.garbageContent}
+            price={popupContents?.price}
+            address={popupContents?.address}
+            image={popupContents?.image}
+            handleClose={handleClose}
+            formatPostTime={formatPostTime}
+            formatPrice={formatPrice}
+            handleOverlayClick={handleOverlayClick}
+            loading={loading}
+        />
+    );
+};
+
+const PopupPresentation = ({ paid, accepted, time, userName, phoneNumber, garbageName, garbageContent, price, address, image, handleClose, formatPostTime, formatPrice, handleOverlayClick, loading}) => {
+    return (
+        <PopupOverlay onClick={handleOverlayClick}>
+            <StyledPopupContainer>
+                <PopupTop>
+                    <PopupTopLeft>
+                        {loading ? null : (accepted ? "승인완료" : "대기중")}
+                    </PopupTopLeft>
+                    <PopupTopRight>
+                        {loading ? null : formatPostTime(time)}
+                        <PopupCloseBtn onClick={handleClose}>X</PopupCloseBtn>
+                    </PopupTopRight>
+                </PopupTop>
+                <PopupMainWrapper>
+                    {loading ? ( 
+                        <LoadingSpinner />
+                    ) : (
+                        <>
+                            <PopupMainContainer>
+                                <PopupMain>
+                                    <StyledDiv>배출자명: <StyledSpan>{userName}</StyledSpan></StyledDiv>
+                                    <StyledDiv>휴대폰 : <StyledSpan>01012345678</StyledSpan></StyledDiv>
+                                    <StyledDiv>폐기물명: <StyledSpan>{garbageName}({garbageContent})</StyledSpan></StyledDiv>
+                                    <StyledDiv>결제금액: <StyledSpan>{formatPrice(price)}원</StyledSpan></StyledDiv>
+                                    <StyledDiv>
+                                        <StyledP>배출장소</StyledP>
+                                        <p><StyledSpan>{address}</StyledSpan></p>    
+                                    </StyledDiv>
+                                </PopupMain>
+                                <PopupMain>
+                                    <ImgWrapper>
+                                        {image && <StyledImg src={`data:image/jpeg;base64,${image}`} alt={garbageName} />}
+                                    </ImgWrapper>
+                                </PopupMain>    
+                            </PopupMainContainer>
+                            <SubmitBtn>승인</SubmitBtn>
+                        </>
+                    )}
+                </PopupMainWrapper>
+            </StyledPopupContainer>
+        </PopupOverlay>
+    );
+};
+
+const PopupOverlay = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+`;
+const StyledPopupContainer = styled.div`
+    background: white;
+    border-radius: 5px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.25);
+    width: 80%;
+    max-width: 740px;
+    min-height: 434px;
+    z-index: 1001;
+`;
+const PopupTop = styled.div`
+    background-color: #FFBB33;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    height: 30px;
+    padding: 10px 16px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: white;
+`;
+const PopupMainWrapper = styled.div`
+    padding: 24px 16px;
+    position: relative;
+    height: 336px;
+    width: calc(100% - 32px);
+`;
+const PopupMainContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+const PopupMain = styled.div`
+    width: 48%;
+    height: 245px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    max-width: 48%;
+`;
+const ImgWrapper = styled.div`
+    width: auto;
+    height: 245px;
+    display: flex;
+    justify-content: end;
+`;
+const StyledImg = styled.img`
+    max-width: 100%;
+    height: 100%;
+`;
+const PopupTopLeft = styled.div`
+    font-size: 20px;
+`;
+const PopupTopRight = styled.div`
+`;
+const StyledDiv = styled.div`
+`;
+const PopupCloseBtn = styled.button`
+    cursor: pointer;
+    margin-left: 16px;
+    width: 24px;
+    height: 24px;
+`;
+const SubmitBtn = styled.button`
+    background-color: #0D6EFD;
+    border-radius: 5px;
+    color: white;
+    width: calc(100% - 32px);
+    height: 48px;
+    font-size: 16px;
+    border: none;
+    position: absolute;
+    bottom: 24px;
+    &:hover{
+        cursor: pointer;
+    };
+`;
+const StyledP = styled.p`
+    padding-bottom: 8px;
+`
+const StyledSpan = styled.span`
+    color: #666666;
+`;
+export default PopupContainer;
--- front/src/pages/AllPosts.js
+++ front/src/pages/AllPosts.js
@@ -2,71 +2,271 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import LoadingSpinner from '../components/Loading';
+import styled from "styled-components";
+import Popup from "../components/AllPostsComponents/Popup"
 
-// API 요청을 별도의 함수로 분리
-const fetchPosts = async (pageNumber, pageSize, setPosts, setErrorLoading, setTotalPages) => {
-  try {
-    const token = localStorage.getItem('token');
-    const response = await axios.get(`https://api.capserver.link/posts?page=${pageNumber}&size=${pageSize}`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-    setPosts(response.data.content);  // 페이지 데이터
-    setTotalPages(response.data.totalPages); // 총 페이지 수
-    setErrorLoading(false); // 데이터 로딩 완료
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    setErrorLoading(false); // 에러 발생 시에도 로딩 상태 갱신
-  }
-};
+function AllPostsContainer() {
+    const [posts, setPosts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const navigate = useNavigate();
+    const pageSize = 15; // 한 페이지당 게시물 수
+    const [postId, setPostId] = useState("")
+    const [showPopup, setShowPopup] = useState(false);
+    const [loading, setLoading] = useState(true);
 
-function AllPosts() {
-  const [posts, setPosts] = useState([]);
-  const [errorLoading, setErrorLoading] = useState(true); // 로딩 상태 관리
-  const [currentPage, setCurrentPage] = useState(0);
-  const [totalPages, setTotalPages] = useState(0);
-  const navigate = useNavigate();
-  const pageSize = 10; // 한 페이지당 게시물 수
+    useEffect(() => {
+        fetchPosts(currentPage, pageSize, setPosts,  setTotalPages);
+    }, [currentPage]);
 
-  useEffect(() => {
-    fetchPosts(currentPage, pageSize, setPosts, setErrorLoading, setTotalPages);
-  }, [currentPage]);
+    const handlePageChange = (newPage) => {
+        if(0<= newPage && newPage < totalPages){
+            setCurrentPage(newPage);
+        }
+    };
 
-  // 페이지 변경 핸들러
-  const handlePageChange = (newPage) => {
-    setCurrentPage(newPage);
-  };
+    const handlePopup = (postId) => {
+        setShowPopup(!showPopup)
+        setPostId(postId)
+    }
 
-  return (
-    <div>
-      {!errorLoading ? (
-        posts.length > 0 ? (
-          posts.map(post => (
-            <div key={post.id} onClick={() => navigate(`/view-by-post/${post.id}`)} style={{ cursor: 'pointer' }}>
-              <h2>폐기물: {post.garbageName}</h2>
-              <p>작성자: {post.username}</p>
-              <p>신청일: {post.time}</p>
-              <p>상태: {post.paid ? (post.accepted ? '승인완료' : '승인대기중') : '미결제'}</p>
-            </div>
-          ))
-        ) : (
-          <p>게시물이 없습니다.</p>
-        )
-      ) : (
-        <LoadingSpinner />
-      )}
-      {totalPages > 1 && (
-        <div>
-          {Array.from({ length: totalPages }, (_, index) => (
-            <button key={index} onClick={() => handlePageChange(index)}>
-              {index + 1}
-            </button>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+    // API 요청을 별도의 함수로 분리
+    const fetchPosts = async (pageNumber, pageSize, setPosts, setTotalPages) => {
+        try {
+            setLoading(true);
+            const token = localStorage.getItem('token');
+            const response = await axios.get(`https://api.capserver.link/posts?page=${pageNumber}&size=${pageSize}`, {
+                headers: {
+                Authorization: `Bearer ${token}`
+                }
+            });
+            setPosts(response.data.content);
+            setTotalPages(response.data.totalPages);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const renderPageNumbers = () => { // 페이징 버튼 생성
+        let totalPagesToShow = totalPages;
+        if (totalPages >= 10) {
+            totalPagesToShow = 9;
+        }
+        const pageRange = Math.floor(totalPagesToShow / 2);
+    
+        let startPage = currentPage - pageRange;
+        let endPage = currentPage + pageRange;
+    
+        if (startPage < 0) {
+            endPage -= startPage;
+            startPage = 0;
+        }
+        if (endPage >= totalPages) {
+            endPage = totalPages - 1;
+            startPage = Math.max(0, totalPages - totalPagesToShow);
+        }
+    
+        return Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index);
+    };
+    const formatPostTime = (time) => {
+        const date = new Date(time);
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        return `${year}.${month < 10 ? '0' + month : month}.${day < 10 ? '0' + day : day}`;
+    };
+    const formatAnonymous = (userid) => {
+        const length = userid.length - 2;
+        userid = userid.slice(0, 2);
+        return `${userid}${'*'.repeat(length)}`;
+    };
+    
+    return (
+        <AllPostsPresentation
+            loading={loading}
+            posts={posts}
+            totalPages={totalPages}
+            handlePageChange={handlePageChange}
+            currentPage={currentPage}
+            renderPageNumbers={renderPageNumbers}
+            formatPostTime={formatPostTime}
+            formatAnonymous={formatAnonymous}
+            handlePopup={handlePopup}
+            postId={postId}
+            setPostId={setPostId}
+            showPopup={showPopup}
+        />
+    );
 }
 
-export default AllPosts;
+const AllPostsPresentation = ({loading, posts, totalPages, handlePageChange, currentPage, renderPageNumbers, formatPostTime, formatAnonymous, handlePopup, postId, setPostId, showPopup }) => {
+    return(
+        <Container>
+        <AllPostsWrapper>
+            <StyledTitle>전체 배출 신청 현황</StyledTitle>
+            <PostListWrapper>
+                <PostListTop />
+                <PostListMain>
+                    {loading ? (
+                        <StyledMessage>
+                            <LoadingSpinner />
+                        </StyledMessage>
+                    ) : (
+                        posts && posts.length > 0 ? (
+                            posts.map(post => (
+                                <Post key={post.id} onClick={() => handlePopup(post.id)}>
+                                    <StyledData width="10%" $justifyContent="center"> 
+                                        <Status
+                                            width="80px"
+                                            height="36px"
+                                            $bgColor={post.accepted ? "#33B5E5" : "#FFBB33"}
+                                        >
+                                            {post.accepted ? "승인완료" : "대기중"}
+                                        </Status>
+                                    </StyledData>
+                                    <StyledData $paddingLeft="3%" width="50%">{post.garbageName}</StyledData>
+                                    <StyledData $justifyContent="end" width="40%">
+                                        {formatAnonymous(post.username)} | {formatPostTime(post.time)}
+                                    </StyledData>
+                                </Post>
+                            ))
+                        ) : (
+                            <StyledMessage>
+                                <p>신청된 폐기물이 없습니다.</p>
+                            </StyledMessage> 
+                        )
+                    )}
+                    </PostListMain>
+                </PostListWrapper>
+                {totalPages > 1 && (
+                    <Paging>
+                        <StyledBtn $width="35px" onClick={() => handlePageChange(0)} $borderTopLeft="5px" $borderBottomLeft="5px">{"<<"}</StyledBtn>
+                        <StyledBtn $width="35px" onClick={() => handlePageChange(currentPage - 1)}>{"<"}</StyledBtn>
+                        {renderPageNumbers().map(number => (
+                            <StyledBtn key={number} onClick={() => handlePageChange(number)} $currentPage={currentPage === number}>
+                                {number + 1}
+                            </StyledBtn>
+                        ))}
+                        <StyledBtn $width="35px" onClick={() => handlePageChange(currentPage + 1)}>{">"}</StyledBtn>
+                        <StyledBtn $width="35px" onClick={() => handlePageChange(totalPages - 1)} $borderTopRight="5px" $borderBottomRight="5px">{">>"}</StyledBtn>
+                    </Paging>
+                )}
+            </AllPostsWrapper>
+            {showPopup && <Popup postId={postId} setPostId={setPostId} handlePopup={handlePopup} />}
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 15px;
+    margin-bottom: 15px;
+`;
+const AllPostsWrapper = styled.div`
+    width: 80%;
+    height: 70%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+const StyledTitle = styled.div`
+    width: 98%;
+    height: 50px;
+    margin: auto;
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+    border-bottom: solid #4DA3D5 5px;
+    color: #4DA3D5;
+    font-size: 25px;
+    font-weight: bold;
+`;
+const PostListWrapper = styled.div`
+    width: 98%;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.25);
+    border-radius: 5px;
+`;
+const PostListTop = styled.div`
+    background-color: #4DA3D5;
+    height: 50px;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+`;
+const PostListMain = styled.div`
+    height: 100%;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+`;
+const Post = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 7px 20px;
+    border-bottom: solid #C7D1D0 1px;
+    &:hover{
+        cursor: pointer;
+    }
+`;
+const Status = styled.div`
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
+    background-color: ${(props) => props.$bgColor};
+    margin-left: ${(props) => props.marginLeft};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 30px;
+    color: white;
+    @media (max-width: 768px) {
+        width: 54px;
+        height: 28px;
+        font-size: 10px;
+}
+`;
+const StyledData = styled.div`
+    width: ${(props) => props.width};
+    text-align: ${(props) => props.$textAlign};
+    padding-left: ${(props) => props.$paddingLeft};
+    display: flex;
+    justify-content: ${(props) => props.$justifyContent};
+    @media (max-width: 768px) {
+        font-size: 12px;
+        padding-left: 0;
+    }
+`;
+
+const Paging = styled.div`
+    padding : 25px 0 10px 0;
+    border-radius: 5px;
+    display: flex;
+`;
+const StyledBtn = styled.button`
+    border: ${(props) => props.$currentPage ? 'solid #6a6a6a 1px' : 'solid #C7D1D0 1px'};    
+    width: ${(props) => props.$width || '30px'}; 
+    height: 30px;
+    background-color: ${(props) => props.$currentPage ? '#024598' : 'white'};
+    color: ${(props) => props.$currentPage ? 'white' : '#0279C2'};
+    box-sizing: border-box;
+    border-top-left-radius: ${(props) => props.$borderTopLeft};
+    border-top-right-radius: ${(props) => props.$borderTopRight};
+    border-bottom-left-radius: ${(props) => props.$borderBottomLeft};
+    border-bottom-right-radius: ${(props) => props.$borderBottomRight};
+    &:hover{
+        cursor: pointer;
+        color: white;
+        background-color: ${(props) => props.$currentPage ? '#024598' : '#4DA3D5'};
+    }
+`;
+const StyledMessage = styled.div`
+    height: calc(100vh - 160px - 100px - 30px - 54.67px - 50px - 65px - 20px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 250px;
+`;
+
+export default AllPostsContainer;
